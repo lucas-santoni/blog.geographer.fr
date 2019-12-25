0aca6f3f3e9cd224ad491efa705ffbf13a93993c
Geographer <geographer@live.fr>
2019-12-25 23:36:15 +0100

    Projects

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,19 +4,19 @@
 from __future__ import unicode_literals
 
 # SITE INFORMATION
-AUTHOR = 'Geographer'
-SITENAME = 'Geographer'
-SITEURL = ''
+AUTHOR = "Geographer"
+SITENAME = "Geographer"
+SITEURL = ""
 
 # THEME
-THEME = 'theme'
+THEME = "theme"
 
 # LINKS
 GITHUB_URL = "https://github.com/geospace"
 TWITTER_URL = "https://twitter.com/geographeur"
 
 # STATIC_PATHS
-STATIC_PATHS = ['assets']
+STATIC_PATHS = ["assets"]
 
 # SITE INTRO
 SITE_INTRO = """
@@ -30,11 +30,11 @@ about CTF writeups, personnal projects, and memos. Please,
 FOOTER_TEXT = "Happily made in 🇫🇷, the most beautiful country in the world..."
 
 # PATH
-PATH = 'content'
+PATH = "content"
 
 # TIME
-TIMEZONE = 'Europe/Paris'
-DEFAULT_LANG = 'en'
+TIMEZONE = "Europe/Paris"
+DEFAULT_LANG = "en"
 
 # FEED
 FEED_ALL_ATOM = None
@@ -66,3 +66,42 @@ SOCIAL = ()
 
 # RELATIVE
 # RELATIVE_URLS = True
+
+
+# PROJECTS
+class project():
+    pass
+
+
+POOL_2019 = project()
+POOL_2019.name = "PoC Security Pool 2018 [French]"
+POOL_2019.emoji = "🏊‍♂️"
+POOL_2019.description = """
+In 2018, I teached ~30 EPITECH students the basics of computer security. This
+is the teaching material I wrote for this occasion.
+<a href="/piscine-poc-2019">Browse it here.</a>
+"""
+
+THIS_BLOG = project()
+THIS_BLOG.name = "This blog"
+THIS_BLOG.emoji = "📖"
+THIS_BLOG.description = """
+This blog is statically generated using a custom Python script. Feel free to
+<a href="https://github.com/Geospace/geoblog">contribute</a>.
+"""
+
+SQLI_PLATFORM = project()
+SQLI_PLATFORM.name = "SQLi Platform"
+SQLI_PLATFORM.emoji = "💉"
+SQLI_PLATFORM.description = """
+A WEB application written in JavaScript that makes it simple to understand
+and visualize SQL injections. Easy to launch via Docker.
+<a href="https://github.com/Geospace/sqli-platform">Get it here.</a>
+"""
+
+# PROJECTS
+PROJECTS = [
+    POOL_2019,
+    THIS_BLOG,
+    SQLI_PLATFORM
+]
