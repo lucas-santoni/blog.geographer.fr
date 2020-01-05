aa5940e514aa80d2f5574ece1d883fd11cd875d2
Geographer <geographer@live.fr>
2020-01-05 16:36:44 +0100

    First test of the dynamic 404 page

---
+++ plugins/api/api.py
@@ -0,0 +1,60 @@
+import os
+
+from pelican import signals
+
+# Generate an "API" for the blog
+# it is actually a JavaScript module that can be easily imported
+# and allows to search for an article
+
+JS_BASE = """const API = [
+  {}
+];
+"""
+
+FILENAME = 'api.js'
+
+
+class APIGenerator():
+    def __init__(self, context, settings, path, theme, output_path):
+        self.context = context
+        self.output_path = output_path
+
+    def generate_output(self, writer):
+        # Final file path
+        path = os.path.join(self.output_path, FILENAME)
+
+        # Extract pages and articles
+        content = \
+            self.context['articles'] + \
+            self.context['pages']
+
+        # Get all the slugs, and titles
+        slugs = [c.slug for c in content]
+        titles = [c.title for c in content]
+
+        # Escape quotes in the title
+        titles = [title.replace('\'', '\\\'') for title in titles]
+
+        # Format objects
+        objs = [
+            f"""{{ title: '{title}', slug: '{slug}' }}"""
+            for title, slug in zip(titles, slugs)
+        ]
+
+        # JavaScript array content
+        js_array_elements = ',\n  '.join(objs)
+
+        # Put content into array
+        js = JS_BASE.format(js_array_elements)
+
+        # Write JS file
+        with open(path, 'w+') as fd:
+            fd.write(js)
+
+
+def get_generators(generators):
+    return APIGenerator
+
+
+def register():
+    signals.get_generators.connect(get_generators)
