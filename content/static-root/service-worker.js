adc66b43fd8362de4ad2046c750f9fecad419459
Geographer <geographer@live.fr>
2020-01-16 17:01:31 +0000

    Increase version

--- content/static-root/service-worker.js
+++ content/static-root/service-worker.js
@@ -2,7 +2,7 @@ const SW_PATH = '/service-worker.js';
 
 const CACHE_COMMON = 'common';
 const CACHE_PAGES = 'pages';
-const CACHE_VERSION = '6::';
+const CACHE_VERSION = '7::';
 
 const DEBUG = false;
 
