630bc7f17de8d251e96bb72bbbadf67a93eb238a
Geographer <geographer@live.fr>
2020-02-04 09:17:37 +0000

    Bump PWA version

--- content/static-root/service-worker.js
+++ content/static-root/service-worker.js
@@ -2,7 +2,7 @@ const SW_PATH = '/service-worker.js';
 
 const CACHE_COMMON = 'common';
 const CACHE_PAGES = 'pages';
-const CACHE_VERSION = '11::';
+const CACHE_VERSION = '12::';
 
 const DEBUG = false;
 
