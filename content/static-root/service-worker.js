ff70403301af1fde8edf10163870ce9215d0a618
Geographer <geographer@live.fr>
2020-07-30 12:53:20 +0200

    Bump

--- content/static-root/service-worker.js
+++ content/static-root/service-worker.js
@@ -2,7 +2,7 @@ const SW_PATH = '/service-worker.js';
 
 const CACHE_COMMON = 'common';
 const CACHE_PAGES = 'pages';
-const CACHE_VERSION = '27::';
+const CACHE_VERSION = '28::';
 
 const DEBUG = false;
 
