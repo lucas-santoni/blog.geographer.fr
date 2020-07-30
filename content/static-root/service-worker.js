7e351d3229880730000fed1e8fa32014fee05e74
Geographer <geographer@live.fr>
2020-07-30 14:05:07 +0200

    Try image large

--- content/static-root/service-worker.js
+++ content/static-root/service-worker.js
@@ -2,7 +2,7 @@ const SW_PATH = '/service-worker.js';
 
 const CACHE_COMMON = 'common';
 const CACHE_PAGES = 'pages';
-const CACHE_VERSION = '29::';
+const CACHE_VERSION = '30::';
 
 const DEBUG = false;
 
