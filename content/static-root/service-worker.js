ebd5d383301d97326ece10600b10f3a4834cab4c
Geographer <geographer@live.fr>
2020-07-30 12:26:11 +0200

    Bump cache version

--- content/static-root/service-worker.js
+++ content/static-root/service-worker.js
@@ -2,7 +2,7 @@ const SW_PATH = '/service-worker.js';
 
 const CACHE_COMMON = 'common';
 const CACHE_PAGES = 'pages';
-const CACHE_VERSION = '26::';
+const CACHE_VERSION = '27::';
 
 const DEBUG = false;
 
